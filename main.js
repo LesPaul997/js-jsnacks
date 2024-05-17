59d9010a17b7c45808b37c061873ba52ab68ef01
LesPaul997 <mariogiordano941@gmail.com>
2024-05-17 20:35:29 +0200

    start : first snack

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+//1 SNACKS
+
+/* //Chiedo all'utente il primo numero
+let firstNumber = prompt ('Digita il primo numero');
+
+//Chiedo all'utente il secondo numero
+let secondNumber = prompt ('Digita il secondo numero');
+
+//Tra i due numeri forniti stampo quello maggiore
+if (firstNumber > secondNumber) {
+    console.log(firstNumber);
+} else if (secondNumber > firstNumber) {
+    console.log(secondNumber)
+} else {
+    console.log('I numeri sono uguali');
+} */
+
+
+//3 SNACKS
+
+//Chiedo all'utente 10 numeri
