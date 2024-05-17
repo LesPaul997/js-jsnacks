22d55349735373439fc48e41e6ac3831a2d10f8f
LesPaul997 <mariogiordano941@gmail.com>
2024-05-17 21:00:30 +0200

    add: 3 snacks

--- main.js
+++ main.js
@@ -19,6 +19,17 @@ if (firstNumber > secondNumber) {
 } */
 
 
+
 //3 SNACKS
 
-//Chiedo all'utente 10 numeri
+//Variabile Somma
+let somma = 0 ;
+
+//Qui inizia il ciclo dei 10 numeri richiesti
+for (let i = 1; i <= 10 ; i++) {
+    somma += +prompt ('Inserisci il numero');
+}
+
+//Stampo il risultato
+console.log(somma);
+
