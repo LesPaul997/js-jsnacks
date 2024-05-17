234c6b56cd0ba32d687faa7a0642710d7d204808
LesPaul997 <mariogiordano941@gmail.com>
2024-05-17 22:22:07 +0200

    add: snack5

---
+++ snack5/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//Crea un array vuoto.
+let array = [""];
+//Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. Stampa l'array alla fine. 
+for (let i = 1; i <= 6 ; i++) {
+    prompt ('Inserisci il numero');
+}
+
+if (number%2 === 0) {
+    console.log(number);
+} else {
+    
+}
+
+//esercio in standby
