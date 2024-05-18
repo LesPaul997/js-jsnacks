b6a665daa2733691f69205123c494af34eb887c0
LesPaul997 <mariogiordano941@gmail.com>
2024-05-18 08:10:00 +0200

    add : snack5 modified

--- snack5/main.js
+++ snack5/main.js
@@ -1,16 +1,16 @@
 'use strict';
 
 //Crea un array vuoto.
-let array = [""];
+const array = [];
 //Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array. Stampa l'array alla fine. 
-for (let i = 1; i <= 6 ; i++) {
-    prompt ('Inserisci il numero');
-}
+for (let i = 0; i <= 5 ; i++) {
+    let num = Number(prompt ('Inserisci il numero'));
 
-if (number%2 === 0) {
-    console.log(number);
-} else {
+//Se è dispari viene trasferito all'interno dell'array
+    if (num % 2) {
+        array.push(num);
     
 }
+}
+    console.log(array);
 
-//esercio in standby
